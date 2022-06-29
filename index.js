88c36d9330d53681a2905b7b66b20bb98d862f12
nguyenbuitung <nguyenbuitung4@gmail.com>
2022-06-29 16:27:51 +0700

    xong bai js 1

---
+++ index.js
@@ -0,0 +1,59 @@
+/** So do 3 khoi
+ * Input:luong 1 ngay,so ngay lam
+ * 
+ * Process:(pseudo code)
+ * 1.Lay dc input(hardcode)
+ * 2.tinh luong=luong 1 ngay * so ngay lam
+ * 3.log luong ra man hinh
+ * 
+ * 
+ * Output:luong
+ * 
+ * 
+ * 
+ * 
+ */
+
+ var salaryPerDay = 100000;
+ var workingDay = 20;
+ var totalsalary = salaryPerDay * workingDay
+ console.log("tong luong trong thang la", totalsalary)
+ 
+ //5 so thuc
+ /**
+  * INPUT:5 so thuc 
+  * 
+  * 
+  * process:
+  * 1.nhap input
+  * 2.trung binh =tong cua 5 so chia cho 5
+  * 
+  * output:gia tri trung binh 5 so thuc
+  */
+ var number1=1
+ var number2=2
+ var number3=3
+ var number4=4
+ var number5=5
+ var aveger=(number1+number2+number3+number4+number5)/5
+ console.log("trung binh=",aveger)
+ //bai 5
+
+ var n=83
+ var num1=n%10
+ var num2=Math.floor(n/10)
+ var sum=num1+num2
+ console.log("tong 2 ky so =",sum)
+ //bai 3
+ var usd=23500
+ var inputusd=3
+ var vnd=inputusd*23500
+ console.log('so tien quy doi ra vnd=',vnd)
+ //bai4
+ var a=29
+ var b=4
+ var area=a*b
+ var perimeter=(a+b)*2
+ console.log("Dien tich hình chữ nhật=",area)
+ console.log("Chu vi hình chữ nhật=",perimeter)
+ 
